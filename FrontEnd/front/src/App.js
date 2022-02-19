23be0ba47dc991ef3eeb69bd6d931d8204cd201a
Monitor Laboratório 3 <monitorlab3@unifacef.local>
2022-02-19 13:02:40 -0300

    A local Todo With React Just to Learn

--- FrontEnd/front/src/App.js
+++ FrontEnd/front/src/App.js
@@ -1,15 +1,51 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList'
+import uuidv4 from 'uuid/v4'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  useState([])
+  const [todos, setTodos] = useState([{}]) // Object Desconstructing
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id){
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+      const name = todoNameRef.current.value
+      if (name === ''){
+        setTodos(prevTodos => {
+          return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+        })
+        todoNameRef.current.value = null
+      }
+  }
+
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
     <>
-      <TodoList />
-      <input type="text" />
-      <button> Add Todo </button>
-      <button> Clear Complete </button>
-      <div> 0 left to do </div>
+      <TodoList todos={todos} toggleTodo = {toggleTodo}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}> Add Todo </button>
+      <button onClick={handleClearTodos}> Clear Complete </button>
+      <div> {todos.filter(todo => !todo.complete).length} Left To Do </div>
     </>
     )
 }
