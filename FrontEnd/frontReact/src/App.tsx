30721916ca488ea537deb515cc79a73ee04a17ca
Pedro Marçal <pedro.h.silva.marcal@gmail.com>
2022-02-20 00:28:49 -0300

    Finish the tutorial

--- FrontEnd/frontReact/src/App.tsx
+++ FrontEnd/frontReact/src/App.tsx
@@ -1,22 +1,45 @@
 import { useState } from 'react'
 import { Tweet } from './components/Tweet' 
 
+import './styles/App.css';
+import { AppRoutes } from './routes/routes';
+
 function App() {
   // Definir um Estado, na qual é definido o tipo dele com <tipo[se é um array]>
-  const [] = useState<string[]>([])
+  const [tweets, setTweets] = useState<string[]>([
+    'Tweet 1',
+    'Tweet 2',
+    'Tweet 3',
+    'Tweet 4',
+  ])
+
+  // Criar uma função dentro do React fora do Return 
+  function createTweet(){
+    setTweets([...tweets, ])
+  }
 
   return (
 
-    <div>
-      <Tweet text="Tweet 1"/>
-      <Tweet text="Tweet 2"/>
-      <Tweet text="Tweet 3"/> 
-
-      <button> Adicionar Tweet</button>
-    </div>
-  )
-}
-
-export default App
+    <AppRoutes />
+    )
+  }
+  
+  export default App
+  
+      // <div>
+      //   {/* Percorre o tweets para retornar no laço de repetição */}
+      //   {tweets.map(tweet => {
+      //     return <Tweet text={tweet}/>
+      //   })}
+        
+      //   <input type="text" />
+      //   <button onClick={createTweet} style={{ 
+      //     backgroundColor: 'B257e6',
+      //     border: 0,
+      //     padding: '6px 12px',
+      //     color: 'e1e1f6'
+      //    }}>
+      //      Adicionar Tweet</button>
+      // </div>
 
 // https://youtu.be/pDbcC-xSat4?t=1195
