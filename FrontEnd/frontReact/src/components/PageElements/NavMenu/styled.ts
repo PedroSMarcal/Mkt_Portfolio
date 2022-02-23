9524014ae4d4f4408cf8547dac03128ac573288f
Monitor Laboratório 3 <monitorlab3@unifacef.local>
2022-02-23 17:32:41 -0300

    Learn Stictches colorts

--- FrontEnd/frontReact/src/components/PageElements/NavMenu/styled.ts
+++ FrontEnd/frontReact/src/components/PageElements/NavMenu/styled.ts
@@ -1,5 +1,10 @@
 import { createStitches } from "@stitches/core";
+import { violet, slate } from '@radix-ui/colors'
 
 export const { css } = createStitches({
-    
+    theme: {
+        colors: {
+            
+        }
+    }
 })
