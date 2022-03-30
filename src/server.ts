60ac3af16f9f8528bb8da102841b57d645375692
Monitor Laboratório 3 <monitorlab3@unifacef.local>
2022-03-30 17:45:00 -0300

    add migration

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,4 @@ import  express  from "express";
 
 const app = express();
 
-app.listen(3333, () => console.log("Server is Running ^o^"));
-
-//https://youtu.be/9AO2hZJsHrs?t=842
+app.listen(3333, () => console.log("Server is Running!"));
